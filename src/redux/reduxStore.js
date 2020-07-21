7adcd993c907a5370f2be68503ebd203c270f45d
Paul Filippov <fpspro@inbox.ru>
2020-07-21 17:29:00 +0300

    подключение redux DevTools. урок 91

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import sidebarReducer from "./sidebarReducer";
@@ -18,7 +18,11 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
+//для расширения redux devtools в chrome
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleWare)));
 
+// let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
+
+window._store_ = store;
 export default store;
-window.store = store;
