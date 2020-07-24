55e4539f0b9dfaa5306b67e4915acade3030e614
Paul Filippov <fpspro@inbox.ru>
2020-07-24 12:09:47 +0300

    Updates

---
+++ precache-manifest.e49cebb56c3932f934429d95937ac921.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50cd7f90c1f9292b476221880f5faa6",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "5047b7446a74766f600a",
+    "url": "/test-deploy/static/css/3.c7b33102.chunk.css"
+  },
+  {
+    "revision": "005e9412a4f9e12ec7fb",
+    "url": "/test-deploy/static/css/4.cbf77934.chunk.css"
+  },
+  {
+    "revision": "c80a58dfb8d6429aa48d",
+    "url": "/test-deploy/static/css/main.8f7d339a.chunk.css"
+  },
+  {
+    "revision": "a0013e0a1b10e8ef9aaa",
+    "url": "/test-deploy/static/js/2.08124050.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-deploy/static/js/2.08124050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5047b7446a74766f600a",
+    "url": "/test-deploy/static/js/3.e05beb01.chunk.js"
+  },
+  {
+    "revision": "005e9412a4f9e12ec7fb",
+    "url": "/test-deploy/static/js/4.371bb55b.chunk.js"
+  },
+  {
+    "revision": "c80a58dfb8d6429aa48d",
+    "url": "/test-deploy/static/js/main.a510233c.chunk.js"
+  },
+  {
+    "revision": "25e6a13fa52a1d752841",
+    "url": "/test-deploy/static/js/runtime-main.e180437a.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/test-deploy/static/media/preloaderGif.26ef8bb4.gif"
+  },
+  {
+    "revision": "d2eddad7785eb23d044e8b425f5c921c",
+    "url": "/test-deploy/static/media/snoop.d2eddad7.png"
+  }
+]);
